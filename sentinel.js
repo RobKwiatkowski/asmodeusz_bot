3b06563aaef8b4a9af5acfa3160fc5bc7d883adb
Robert Kwiatkowski <robert.kwiatkowski01@gmail.com>
2026-05-21 18:20:42 +0200

    Dodaj plik startowy Sentinel

---
+++ sentinel.js
@@ -0,0 +1,20 @@
+// Punkt startowy bota Sentinel. Cala logika jest ladowana z modulow,
+// dzieki czemu ten plik odpowiada tylko za start aplikacji.
+const { config } = require('./src/config');
+const { createDiscordClient } = require('./src/client');
+const { setupCommands } = require('./src/commandLoader');
+const { setupEvents } = require('./src/eventLoader');
+const { setupFeatures } = require('./src/features');
+
+if (!config.discord.token) {
+  console.error('Brakuje DISCORD_TOKEN. Uzupelnij .env albo zmienne srodowiskowe kontenera.');
+  process.exit(1);
+}
+
+const client = createDiscordClient();
+
+setupCommands(client);
+setupFeatures(client);
+setupEvents(client);
+
+client.login(config.discord.token);
